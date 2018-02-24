8ec88418be817842b4ecf8910909a3f2fa463575
Bill Huggins <wjhuggins@gmail.com>
2018-02-23 22:35:07 -0800

    Fixed bugs, I think. I left the print statements in so that you could see my process. I'd look over the changes before removing them.

--- QuickCircuit.cc
+++ QuickCircuit.cc
@@ -172,6 +172,7 @@ public:
             ancilla.set(anIndex(1),1);
             env *= ancilla;
         }
+
         //VLADIMIR: Here, the variable "bound" determines how far along a level we go.
         //I simply change bound when current level == target level, to avoid writing
         // a separate but near identical case for that. Likewise for ContractUp.
@@ -183,6 +184,7 @@ public:
             }
             currentLevel--;
         }
+
         return env;
     }
 
@@ -216,11 +218,14 @@ public:
             to_return *= delta(my_indices_[0][i], mpo_index);
         }
         int currentLevel = 0;
-        int bound = 0;
+        int bound = -1;
+        Print(to_return);
         while(currentLevel<= level){
             if(level==currentLevel){bound = gate_position;}
-            for(int i=num_qubits_/2 -1; i > bound; i--){
+            for(int i=num_qubits_/2 - 1; i > bound; i--){
                 to_return *= my_gates_[currentLevel][i];
+                Print(my_gates_[currentLevel][i]);
+                Print(to_return);
             }
             currentLevel++;
         }
@@ -253,7 +258,8 @@ public:
         Print(enviro);
 
         IndexSet ii = enviro.inds();
-        ITensor U(my_indices_[level+1][2*gate_position],my_indices_[level+1][(2*gate_position+1)%num_qubits_]), S, V;
+        int x_pos = 2 * gate_position + (1 + level) % 2;
+        ITensor U(my_indices_[level+1][x_pos],my_indices_[level+1][x_pos + 1]), S, V;
         svd(enviro, U,S,V);
         Index us = commonIndex(U,S);
         Index sv = commonIndex(S,V);
@@ -322,12 +328,20 @@ sanityChecksForSimpleNetwork() {
         ampo += 0.5,"S-",j,"S+",j+1;
     }
     auto H = MPO(ampo);
-    ITensor d = network_2.contractUp(0,0,H);
-    ITensor u = network_2.contractDown(0,1);
+    ITensor u = network_2.contractDown(1,1);
+    ITensor d = network_2.contractUp(1,1,H);
+
+    Print(u);
+    Print(d);
+
+    Print(u * d);
     network_2.updateGate(2,1,H);
-    ITensor env = network_2.contractUp(0,0,H);
-    ITensor gate = network_2.contractDown(0,1);
-    return rank(env*gate)-rank(u*d);
+    
+    ITensor u_new = network_2.contractDown(1,1);
+    ITensor d_new = network_2.contractUp(1,1,H);
+
+
+    return rank(u*d) - rank(u_new*d_new);
 
 }
 
